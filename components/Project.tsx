35accf6ee22d67c9303f305284d5e8115d1008ed
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-04-16 23:36:51 +0530

    update css

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,7 +4,7 @@ import ProjectCard from './ProjectCard'
 const Project = () => {
   return (
     <> 
-    <h1 className='text-center pt-4 text-4xl text-white pb-12 '>Projects</h1>
+    <h1 className='text-center pt-4 text-4xl text-white pb-12 '>My Projects</h1>
     <div className='flex flex-wrap gap-14 '>
     {PROJECT_DATA.map((items, index) =>(
                  <ProjectCard key={index} {...items}/>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,25 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
-
-interface projectCard{
-    title: string,
-    description: string,
-    img: string,
-    tag: string
+interface projectCard {
+  title: string,
+  description: string,
+  img: string,
+  tag: string
 }
-
-const ProjectCard = ({title, description,img, tag}:projectCard) => { 
-   
+const ProjectCard = ({ title, description, img, tag }: projectCard) => {
   return (
     <>
-        <div className='w-[250px] border-2 border-purple-500 rounded-xl px-7 bg-[#181818] '>
-          <div className='flex justify-center items-center pt-2'>
-          <Image src={img} alt='hello' height={220} width={150}/>
-          </div>
-            
-            <p className='text-white text-2xl pt-5 text-center'>{title}</p>
-            <p className='text-white text-lg pt-3 pb-5'>{description}</p>
+      <div className='w-[250px] border-2 border-purple-500 rounded-xl  bg-[#181818] '>
+        <div className='h-52 w-full md:h-56 rounded-t-xl relative group'
+          style={{ background: `url(${img})`, backgroundSize: "cover" }}>
         </div>
+        <div className='text-white px-5'>
+        <p className='text-2xl pt-5 text-center'>{title}</p>
+        <p className='text-lg pt-3 pb-5'>{description}</p>
+        </div>
+      </div>
     </>
   )
 }
