e3dc788cdc3f72c96c0be74752b6cc3113947a58
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-04-17 22:44:17 +0530

    adding footer setion

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer> 
+        <div className='text-white bg-[#121212] px-5 pt-3'>
+        <span>LOGO</span>
+        <p className='text-center text-sm pt-5 pb-5'>All right reserved</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
