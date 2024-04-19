cc0891e97c05f4a1143255509efff2cd522a0af3
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-04-20 00:24:32 +0530

    up

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,9 +1,21 @@
 import { EmailTemplate } from '@/components/EmailTemplate';
+import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const  Emailfrom=process.env.FROM_EMAIL as string;
+
 export async function POST() {
+  if(!resend){
+    return NextResponse.json({
+      message:"we did not send email"
+    })
+  }
+  if(!Emailfrom){
+    return NextResponse.json({
+      message:"we did not send email"
+    })
+  }
   try {
     const data = await resend.emails.send({
       from:Emailfrom,
