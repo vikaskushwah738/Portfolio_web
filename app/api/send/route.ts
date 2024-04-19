50d3cbe09e03a1bd878cd3716249b2a0df7322ec
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-04-20 00:02:57 +0530

    update

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,19 +1,19 @@
-import { EmailTemplate } from '@/components/EmailTemplate';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '@/components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const  Emailfrom=process.env.FROM_EMAIL as string;
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from:Emailfrom,
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const  Emailfrom=process.env.FROM_EMAIL as string;
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from:Emailfrom,
+//       to: ['delivered@resend.dev'],
+//       subject: 'Hello world',
+//       react: EmailTemplate({ firstName: 'John' }),
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
