c85850586637277e3790dc4be44f7b83fa4dc7cd
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-27 12:58:37 +0530

    small changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+
 
 const Footer = () => {
   return (
-    <footer className=' text-white bg-[#121212] px-5  footer border border-t-[#33353f] border-r-transparent border-l-transparent'> 
+    <footer className='text-white bg-[#121212] px-5  footer border border-t-[#33353f] border-r-transparent border-l-transparent'> 
         <div className='p-10 container'>
         <span>LOGO</span>
         <p className='text-center text-sm pt-5 pb-1'>All right reserved</p>
--- data/data.js
+++ data/data.js
@@ -40,73 +40,11 @@
  export const PROJECT_DATA = [
     {
       id: 1,
-      title: "Item 1",
+      title: "Eclat Engineering Consultants",
       description: "Description of Item 1",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag1"
+      img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735284257/Screenshot_2024-12-27_125204_r8gvzz.png",
+      
     },
-    {
-      id: 2,
-      title: "Item 2",
-      description: "Description of Item 2",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag2"
-    },
-    {
-      id: 3,
-      title: "Item 3",
-      description: "Description of Item 3",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag3"
-    },
-    {
-      id: 4,
-      title: "Item 4",
-      description: "Description of Item 4",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag4"
-    },
-    {
-      id: 5,
-      title: "hello i am vikas kushwa h i ahelp you ",
-      description: "Description of Item 5",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag5"
-    },
-    {
-      id: 6,
-      title: "Item 6",
-      description: "Description of Item 6",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag6"
-    },
-    {
-      id: 7,
-      title: "Item 7",
-      description: "Description of Item 7",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag7"
-    },
-    {
-      id: 8,
-      title: "Item 8",
-      description: "hello i an best offer for your",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag8"
-    },
-    {
-      id: 9,
-      title: "Item 9",
-      description: "Description of Item 9",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag9"
-    },
-    {
-      id: 10,
-      title: "Item 10",
-      description: "Description of Item 10",
-      img: "https://images.pexels.com/photos/4835429/pexels-photo-4835429.jpeg?auto=compress&cs=tinysrgb&w=600",
-      tag: "tag10"
-    }
+   
   ];
   
