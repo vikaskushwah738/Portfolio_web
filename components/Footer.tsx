ddcd51e266ea027c6393045c720d1114afeacd69
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-27 14:50:58 +0530

    minner changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='text-white bg-[#121212] px-5  footer border border-t-[#33353f] border-r-transparent border-l-transparent'> 
         <div className='p-10 container'>
-        <span>LOGO</span>
+        <p className="text-lg md:text-xl text-white font-semibold">Vikas</p>
         <p className='text-center text-sm pt-5 pb-1'>All right reserved</p>
         </div>
     </footer>
