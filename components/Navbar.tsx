db757d980653dc45e955ea8950090b2c5bac2a20
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-27 13:03:05 +0530

    navbar updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,8 +31,8 @@ const Navbar = () => {
             <div className='flex flex-wrap w-full items-center
       justify-between mx-auto h-14 md:px-12 px-5 cursor-pointer'>
                 <Link href={"/"}
-                    className='text-lg md:text-3xl text-white font-semibold'>
-                    Logo
+                    className='text-lg md:text-3xl text-white font-semibold '>
+                    <i>Vikas</i> 
                 </Link>
                 <div className="menu hidden md:w-auto md:block ">
                     <ul className='space-x-5 md:flex-row md:flex cursor-pointer'>
