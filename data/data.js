36946b32dc2ac217f807c6fa3ef0127c45c65963
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-28 10:04:32 +0530

    small changes

--- data/data.js
+++ data/data.js
@@ -1,69 +1,63 @@
- const TAB_DATA = [
-    {
-      title: "skills",
-      id: "skills",
-      content:  (
-        <ul className="list-disc pl-2">
-          <li>Javascript</li>
-          <li>Node.js</li>
-          <li>Next.js</li>
-          <li>Tailwin CSS</li>
-          <li>Mongodb</li>
-          <li>Prisma</li>
-          <li>Git</li>
-          <li>TypeScript</li>
-          <li>Rest API</li>
-        </ul>
-      ),
-    },
-    {
-        title: "Education",
-        id: "education",
-        content:  (
-          <ul className="list-disc pl-2">
-            <li>St. Mary's Groups of institution Hyderabad</li>
-          </ul>
-        )
-      },
-      {
-        title: "Cert",
-        id: "cert",
-        content: (
-          <ul className="list-disc pl-2">
-            <li>Responsive Web Design - freecodecamp</li>
-            
-          </ul>
-        ),
-      },
-    
-  ];
-  export default TAB_DATA ;
-
- export const PROJECT_DATA = [
+const TAB_DATA = [
   {
-    id: 4,
+    title: "skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Javascript</li>
+        <li>Node.js</li>
+        <li>Next.js</li>
+        <li>Tailwin CSS</li>
+        <li>Mongodb</li>
+        <li>Prisma</li>
+        <li>Git</li>
+        <li>TypeScript</li>
+        <li>Rest API</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>St. Mary's Groups of institution Hyderabad</li>
+      </ul>
+    )
+  },
+  {
+    title: "Cert",
+    id: "cert",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Responsive Web Design - freecodecamp</li>
+
+      </ul>
+    ),
+  },
+
+];
+export default TAB_DATA;
+
+export const PROJECT_DATA = [
+  {
+    id: 1,
     title: "Eclat Engineering Consultants",
     img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735360120/eclat_fququs.png",
-    link:'https://www.eclatengineering.com/'
+    link: 'https://www.eclatengineering.com/'
   },
-    {
-      id: 1,
-      title: "Eclat Engineering Consultants",
-      img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735289291/Untitled_design_bzd26q.png",
-      link:'https://www.eclatengineering.com/'
-    },
-    {
-      id: 2,
-      title: "Sommet Infra Projects PVT LID",
-      img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735358615/sommet-infra_mkuhdd.png",
-      link:'https://www.eclatengineering.com/'
-    },
-    {
-      id: 3,
-      title:"So Efforts Solution",
-      img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735290531/Untitled_design_2_rjmigk.png",
-      link:'https://www.'
-    },
-   
-  ];
-  
+
+  {
+    id: 2,
+    title: "Sommet Infra Projects PVT LID",
+    img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735360123/sommet_o29ule.png",
+    link: 'https://www.eclatengineering.com/'
+  },
+  {
+    id: 3,
+    title: "So Efforts Solution",
+    img: "https://res.cloudinary.com/dfbm8in4c/image/upload/v1735290531/Untitled_design_2_rjmigk.png",
+    link: 'https://www.'
+  },
+
+];
