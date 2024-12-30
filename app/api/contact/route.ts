c861202ebd92db4e53e53a9d9e8953fa4a199947
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-30 12:37:12 +0530

    p

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/prisma";
 
-
 export async function POST(req: Request){
     try{
         const {name, email , subject , message}= await req.json();
