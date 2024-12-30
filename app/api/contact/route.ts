d715ed010ba1f9fa3e10284c1f2ee71152594e08
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-30 12:36:51 +0530

    minner updtaes

---
+++ app/api/contact/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/prisma";
+
+
+export async function POST(req: Request){
+    try{
+        const {name, email , subject , message}= await req.json();
+        if(!name || !email || !subject || !message){
+            return NextResponse.json({error : 'email and name is mondory for submit form'},
+                {status : 400}
+            )
+        }
+        const response= await prisma.clientData.create({
+            data:{
+                name, 
+                email, 
+                subject, 
+                message
+            }
+        })
+        return NextResponse.json(response);
+    } catch (error){
+        console.log('somethink went wrong', error)
+        return NextResponse.json({error : "somethink went wrong, try again later"}, {status : 500})
+    } finally {
+        await prisma.$disconnect();
+      }
+}
+
+
+
