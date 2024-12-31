03b4e6bf365d25482ab7a854b1f1be4c0cb4795f
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-31 15:20:46 +0530

    cotact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,13 +1,42 @@
 "use client"
 import { useState } from "react"
+import toast from "react-hot-toast";
+import { PhoneInput } from "react-international-phone";
 
 export const ContactForm = () => {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [subject, setSubject] = useState();
-    const [message, setMessage] = useState();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const toastId = toast.loading("Please wait...");
+        try {
+            const res = await fetch(`/api/client-data`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name, phone, email, message }),
+            });
+
+            if (res.ok) {
+                toast.success("Successfully submitted! We’ll contact you soon.", { id: toastId });
+                setName("");
+                setPhone("");
+                setEmail("");
+                setMessage("");
+
+            } else {
+                const errorData = await res.json();
+                toast.error(errorData.message || "Submission failed.", { id: toastId });
+            }
+        } catch (error) {
+            console.log('somethink went wrong', error)
+            toast.error("Network error. Please try again later.", { id: toastId });
+        }
+    };
     return (
-        <form className='flex flex-col'>
+        <form className='flex flex-col' onSubmit={handleSubmit}>
             <div className='mb-6'>
                 <label
                     htmlFor='fname'
@@ -19,6 +48,8 @@ export const ContactForm = () => {
                     id='fname'
                     name='fname'
                     required
+                    value={name}
+                    onChange={(e) => setName(name)}
                     placeholder='Enter your name'
                     className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
                 />
@@ -33,6 +64,8 @@ export const ContactForm = () => {
                     type="email"
                     id='email'
                     required
+                    value={email}
+                    onChange={(e) => setEmail(email)}
                     placeholder='jdjbdjb@gmail.com'
                     className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
                 />
@@ -41,14 +74,15 @@ export const ContactForm = () => {
                 <label
                     htmlFor='subject'
                     className='mb-2 text-white block text-sm font-medium'>
-                    Subject
+                    Phone Number
                 </label>
-                <input
-                    type="text"
-                    id='subject'
-                    required
-                    placeholder='Just say hi'
-                    className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
+                <PhoneInput
+                    defaultCountry="in"
+                    value={phone}
+                    inputStyle={{ backgroundColor: "#F3F4F6", border: "none", fontSize: "16px", paddingLeft: "6px" }}
+                    onChange={(phone) => setPhone(phone)}
+                    placeholder="Phone Number"
+                    className="w-full border border-gray-300 rounded-xl focus:outline-none bg-gray-100"
                 />
             </div>
             <div>
@@ -60,6 +94,8 @@ export const ContactForm = () => {
                 <textarea
                     id='message'
                     required
+                    value={message}
+                    onChange={(e) => setMessage(message)}
                     placeholder='Message'
                     className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
                 />
