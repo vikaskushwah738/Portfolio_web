46c07e29af4c01e8b2fe39895d680cb8945f4220
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-31 15:03:46 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,17 @@
+import AboutSection from "@/components/AboutSection";
+import { EmailSection } from "@/components/EmailSection";
+import HeroSection from "@/components/HeroSection";
+import Project from "@/components/Project";
 
-
-const Home = () => {
+export default function Home() {
   return (
-    <main>
-      <h1>Ai chat bot</h1>
+    <main className="flex min-h-screen flex-col bg-black">
+          <HeroSection/>
+          <AboutSection/>
+          <Project/>
+          <EmailSection />
     </main>
-  )
+  );
 }
-
-export default Home
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import AboutSection from "@/components/AboutSection";
-// import { EmailSection } from "@/components/EmailSection";
-// import HeroSection from "@/components/HeroSection";
-// import Project from "@/components/Project";
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col bg-black">
-//           <HeroSection/>
-//           <AboutSection/>
-//           <Project/>
-//           <EmailSection />
-//     </main>
-//   );
-// }
 
 
