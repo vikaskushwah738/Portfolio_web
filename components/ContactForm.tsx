d642b1c4b115b78dbbe427bc05b164a64568a7cf
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-31 15:05:50 +0530

    remove code

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,72 +2,72 @@
 import { useState } from "react"
 
 export const ContactForm = () => {
-    const [name, setName]=useState();
-    const [email, setEmail]=useState();
-    const [subject, setSubject]=useState();
-    const [message, setMessage]=useState();
-  return (
-    <form className='flex flex-col'>
-        <div className='mb-6'>
-                        <label
-                            htmlFor='fname'
-                            className='mb-2 text-white block text-sm font-medium'>
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id='fname'
-                            name='fname'
-                            required
-                            placeholder='Enter your name'
-                            className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
-                        />
-                    </div>
-                    <div className='mb-6'>
-                        <label
-                            htmlFor='email'
-                            className='mb-2 text-white block text-sm font-medium'>
-                            Your E-mail
-                        </label>
-                        <input
-                            type="email"
-                            id='email'
-                            required
-                            placeholder='jdjbdjb@gmail.com'
-                            className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
-                        />
-                    </div>
-                    <div className='mb-6'>
-                        <label
-                            htmlFor='subject'
-                            className='mb-2 text-white block text-sm font-medium'>
-                            Subject
-                        </label>
-                        <input
-                            type="text"
-                            id='subject'
-                            required
-                            placeholder='Just say hi'
-                            className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
-                        />
-                    </div>
-                    <div>
-                        <label
-                            htmlFor='message'
-                            className='mb-2 text-white text-sm block font-medium'>
-                            Massage
-                        </label>
-                        <textarea
-                            id='message'
-                            required
-                            placeholder='Message'
-                            className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
-                        />
-                    </div>
-                    <button
-                        type='submit'
-                        className='bg-purple-500 rounded-lg md:mx-28  mx-auto hover:bg-purple-600 text-white font-medium py-2 w-1/3 mt-6'
-                    >Submit</button>
-                </form>
-  )
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [subject, setSubject] = useState();
+    const [message, setMessage] = useState();
+    return (
+        <form className='flex flex-col'>
+            <div className='mb-6'>
+                <label
+                    htmlFor='fname'
+                    className='mb-2 text-white block text-sm font-medium'>
+                    Name
+                </label>
+                <input
+                    type="text"
+                    id='fname'
+                    name='fname'
+                    required
+                    placeholder='Enter your name'
+                    className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
+                />
+            </div>
+            <div className='mb-6'>
+                <label
+                    htmlFor='email'
+                    className='mb-2 text-white block text-sm font-medium'>
+                    Your E-mail
+                </label>
+                <input
+                    type="email"
+                    id='email'
+                    required
+                    placeholder='jdjbdjb@gmail.com'
+                    className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
+                />
+            </div>
+            <div className='mb-6'>
+                <label
+                    htmlFor='subject'
+                    className='mb-2 text-white block text-sm font-medium'>
+                    Subject
+                </label>
+                <input
+                    type="text"
+                    id='subject'
+                    required
+                    placeholder='Just say hi'
+                    className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='message'
+                    className='mb-2 text-white text-sm block font-medium'>
+                    Massage
+                </label>
+                <textarea
+                    id='message'
+                    required
+                    placeholder='Message'
+                    className='bg-[#18191E] broder broder-[#33353F] rounded-md placeholder-[#9CA2A9] text-gray-100 text-sm block p-2.5 w-full md:w-3/4  '
+                />
+            </div>
+            <button
+                type='submit'
+                className='bg-purple-500 rounded-lg md:mx-28  mx-auto hover:bg-purple-600 text-white font-medium py-2 w-1/3 mt-6'
+            >Submit</button>
+        </form>
+    )
 }
--- components/EmailTemplate.tsx
+++ components/EmailTemplate.tsx
@@ -1,14 +1,10 @@
-import * as React from 'react';
-interface EmailTemplateProps {
-  firstName: string;
- 
-}
-export const EmailTemplate= ({firstName}:EmailTemplateProps) =>{
+
+export const EmailTemplate= () =>{
    
     return (
         <div>
             <h1>h</h1>
-            <h1>Welcome, {firstName}!</h1>
+            <h1>Welcome, !</h1>
             <p>Thank you for contacting us!</p>
             <p>New message submitted</p>
             <p>j</p>
